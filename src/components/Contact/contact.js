50268902b001221af13bb11435f2c62390f302b8
benistr <benjamin.str@gmail.com>
2020-06-02 18:35:11 +0200

    contact form is working with emailjs

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -3,6 +3,7 @@ import './contact.scss';
 import '../../styles/_vars.sass';
 import Stain2 from './Stain2.png';
 import Stain3 from './Stain3.png';
+import * as emailjs from 'emailjs-com';
 
 class Contact extends React.Component {
     constructor(props) {
@@ -11,34 +12,12 @@ class Contact extends React.Component {
         name: '',
         email: '',
         message: ''
-      }
+      };
+      this.onNameChange = this.onNameChange.bind(this);
+      this.onEmailChange = this.onEmailChange.bind(this);
+      this.onMessageChange = this.onMessageChange.bind(this);
     }
-  
-  render() {
-   return(
-     <div className="container">
-     <form id="contact-form" onSubmit={this.handleSubmit.bind(this)} method="POST">
-      <div className="form-group">
-          <label htmlFor="name">Full Name</label>
-          <input type="text" placeholder="Please type your full name" className="form-control" value={this.state.name} onChange={this.onNameChange.bind(this)} />
-      </div>
-      <div className="form-group">
-          <label htmlFor="exampleInputEmail1">Email address</label>
-          <input type="email" placeholder="Please type your email adress" className="form-control" aria-describedby="emailHelp" value={this.state.email} onChange={this.onEmailChange.bind(this)} />
-      </div>
-      <div className="form-group">
-          <label htmlFor="message">Message</label>
-          <textarea className="form-control" placeholder="Please type your message here if you have a job for me, a million dollar to give me, or if you just want to say hi&#9786;" rows="5" value={this.state.message} onChange={this.onMessageChange.bind(this)} />
-      </div>
-      <button type="submit" className="btn btn-primary">Submit</button>
-      </form>
 
-      <img className="stain2" src={Stain2}></img>
-      <img className="stain3" src={Stain3}></img>
-      </div>
-   );
-  }
-  
     onNameChange(event) {
       this.setState({name: event.target.value})
     }
@@ -52,7 +31,50 @@ class Contact extends React.Component {
     }
   
   handleSubmit(event) {
+    emailjs.sendForm(
+      "gmail",
+      "contact_form",
+      ".contact-form",
+      "user_5OMUCSvwmqYc79K2f5H8x"
+    )
+    .then(function(){
+      alert("Sent!");
+    })
+    .catch(function(){
+      alert("Send email failed!");
+    });
+    this.setState({
+      name: '',
+      email: '',
+      message: '',
+    });
   }
+  
+  render() {
+   return(
+     <div className="container">
+     <form className="contact-form" onSubmit={this.handleSubmit.bind(this)}>
+      <div className="form-group">
+          <label htmlFor="name">Full Name</label>
+          <input type="text" placeholder="Please type your full name" className="form-control" value={this.state.name} name="name" onChange={this.onNameChange.bind(this)} />
+      </div>
+      <div className="form-group">
+          <label htmlFor="exampleInputEmail1">Email address</label>
+          <input type="email" placeholder="Please type your email adress" className="form-control" aria-describedby="emailHelp" value={this.state.email} name="email" onChange={this.onEmailChange.bind(this)} />
+      </div>
+      <div className="form-group">
+          <label htmlFor="message">Message</label>
+          <textarea className="form-control" placeholder="Please type your message here if you have a job for me, a million dollar to give me, or if you just want to say hi&#9786;" rows="5" value={this.state.message} name="message" onChange={this.onMessageChange.bind(this)} />
+      </div>
+      <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
+
+      <img className="stain2" src={Stain2}></img>
+      <img className="stain3" src={Stain3}></img>
+      </div>
+   );
+  }
+  
   }
   
   export default Contact;
